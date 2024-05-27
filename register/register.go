21cdec0cdbafc4608a99ec4e566e1b5381e0bb13
mohamed.agoumi <agoumihunter@gmail.com>
2024-05-27 17:43:37 +0100

    initial database insert (still need to update according to the docs)

--- register/register.go
+++ register/register.go
@@ -1,6 +1,9 @@
 package register
 
 import (
+	"encoding/json"
+	"magoumi/spotify-account/config"
+	"magoumi/spotify-account/model"
 	"net/http"
 )
 
@@ -9,6 +12,29 @@ type Register struct{}
 
 // Register handles the HTTP request for user registration.
 func (h *Register) Register(w http.ResponseWriter, r *http.Request) {
+	// Load the body
+	var user model.User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+
+	// Check if email is taken
+	var existingUser model.User
+	config.DB.Where("email = ?", user.Email).First(&existingUser)
+
+	if existingUser.ID != 0 {
+		http.Error(w, "User already registered", http.StatusBadRequest)
+		return
+	}
+
+	result := config.DB.Create(&user)
+	if result.Error != nil {
+		http.Error(w, "Something went wrong, please try again later", http.StatusInternalServerError)
+		return
+	}
+
 	w.WriteHeader(http.StatusCreated)
 	_, _ = w.Write([]byte("User registered successfully"))
 }
