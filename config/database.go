2957dbd67c0a6d5ecf3212d8fc2fa6a4c0ef451a
mohamed.agoumi <agoumihunter@gmail.com>
2024-05-27 11:24:46 +0100

    add connect to database configs

---
+++ config/database.go
@@ -0,0 +1,29 @@
+// config/database.go
+package config
+
+import (
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"os"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	dbHost := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbPort := os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=UTC", dbHost, dbUser, dbPassword, dbName, dbPort)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	DB = db
+	log.Println("Database connection established.")
+}
