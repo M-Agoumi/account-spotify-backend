d7388587ad4e0b9cf0fbe577c714c4ba91ebc8e1
mohamed.agoumi <agoumihunter@gmail.com>
2024-05-27 11:25:04 +0100

    connect to db and run migrations

--- main.go
+++ main.go
@@ -3,11 +3,27 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/joho/godotenv"
+	"log"
 	"magoumi/spotify-account/application"
+	"magoumi/spotify-account/config"
+	"magoumi/spotify-account/model"
 )
 
 func main() {
 	app := application.New()
+
+	errDot := godotenv.Load()
+	if errDot != nil {
+		log.Println("Error loading .env file")
+	}
+
+	config.ConnectDatabase() // Initialize the database connection
+	errMigration := config.DB.AutoMigrate(&model.User{})
+
+	if errMigration != nil {
+		return
+	} // Automatically migrate the schema
 
 	err := app.Start(context.TODO())
 	if err != nil {
