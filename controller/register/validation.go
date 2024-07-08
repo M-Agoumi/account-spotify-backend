b5e979f3517a0118d669cc54bcb73a73656178f6
mohamed.agoumi <agoumihunter@gmail.com>
2024-07-08 15:35:52 +0100

    update code to go with models refactor

--- controller/register/validation.go
+++ controller/register/validation.go
@@ -2,11 +2,11 @@ package register
 
 import (
 	"fmt"
-	"github.com/M-Agoumi/account-spotify-backend/model"
+	"github.com/M-Agoumi/account-spotify-backend/model/user"
 )
 
 // validateUserEmail checks if the required fields are present and not empty
-func validateUserEmail(user model.User) error {
+func validateUserEmail(user user.User) error {
 	if user.Email == nil || *user.Email == "" {
 		return fmt.Errorf("email is required")
 	}
